73ffb6ab0cb1d3d0ffa55560882fda6802b27632
oliver <lin.cheng@wmich.edu>
2016-02-07 14:21:16 -0500

    nth

--- ticket.sh
+++ ticket.sh
@@ -7,6 +7,7 @@ AUT_MAINCLASS="examples.ticket.Ticket"
 guiFile=$AUTDIR"/"$AUT".GUI"
 efgFile=$AUTDIR"/"$AUT".EFG"
 logFile=$AUTDIR"/"$AUT".log"
+log4j="./src/main/resources/log4j.properties"
 
 classpath="."
 for jar in `ls lib`
@@ -32,16 +33,16 @@ mkdir branches/
 touch inputs
 mkdir $AUTDIR"/testcases"
 
-ripperCmd="java -Dlog4j.configuration=$logFile -cp $classpath edu.umd.cs.guitar.ripper.JFCRipperMain -c $AUT_MAINCLASS -g $guiFile -cf $configurationFile -d 500 -i 2000 -l $logFile"
+ripperCmd="java -Dlog4j.configuration=$log4j -cp $classpath edu.umd.cs.guitar.ripper.JFCRipperMain -c $AUT_MAINCLASS -g $guiFile -cf $configurationFile -d 500 -i 2000 -l $logFile"
 
 eval $ripperCmd
 
-gui2efgCmd="java -Dlog4j.configuration=$logFile -cp $classpath  edu.umd.cs.guitar.graph.GUIStructure2GraphConverter -p EFGConverter -g $guiFile -e $efgFile"
+gui2efgCmd="java -Dlog4j.configuration=$log4j -cp $classpath  edu.umd.cs.guitar.graph.GUIStructure2GraphConverter -p EFGConverter -g $guiFile -e $efgFile"
 
 eval $gui2efgCmd
 
 #testcaseCmd="java -Dlog4j.configuration=$logFile -cp $classpath  edu.umd.cs.guitar.testcase.TestCaseGenerator -p RandomSequenceLengthCoverage -e $efgFile -l 1 -m 200 -d $AUTTESTCASE"
-testcaseCmd="java -Dlog4j.configuration=$logFile -cp $classpath  edu.umd.cs.guitar.testcase.TestCaseGenerator -p BytecodeAnalysis  -e $efgFile -l 2 -m 200 -d $AUTTESTCASE --scope ./aut/radioButton.jar  --method pair --shared 0"
+testcaseCmd="java -Dlog4j.configuration=$log4j -cp $classpath  edu.umd.cs.guitar.testcase.TestCaseGenerator -p BytecodeAnalysis  -e $efgFile -l 2 -m 200 -d $AUTTESTCASE --scope ./aut/radioButton.jar  --method pair --shared 0"
 echo $testcaseCmd
 eval $testcaseCmd
 

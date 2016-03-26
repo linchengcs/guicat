b3c7661480e30a71b068a4446d901ca7701e329e
oliver <lin.cheng@wmich.edu>
2016-03-26 14:27:42 -0400

    coverage guicat

--- jacoco.sh
+++ jacoco.sh
@@ -1,17 +1,31 @@
-#aut id
-
-if [ -z $1 ]; then
-    echo "Usage: $0 autId"
+#check args
+if [ -z $1 ] || [ -z $2 ]; then
+    echo "Usage: $0 autId guitar|guicat"
     exit
 fi
 
+#globals
 aut=$1
-
+testMethod=$2
 #source config file
 . ./conf/$aut/$aut.conf
 
 
+#logic
+if [ "$testMethod" = "guitar" ] ; then
+    testcaseDir=$AUTTESTCASE
+    reportDir=$guitarReportDir
+    reportJacocoExecDir=$guitarReportJacocoExecDir
+elif [ "$testMethod" = "guicat" ] ; then
+    testcaseDir=$autGuicatTestcase
+    reportDir=$guicatReportDir
+    reportJacocoExecDir=$guicatReportJacocoExecDir
+else
+    echo "Usage: $0 autId guitar|guicat"
+    exit
+fi
 
+rm -rf $reportDir
 if [ ! -d "$guitarReportJacocoExecDir" ]; then
     mkdir -p "$guitarReportJacocoExecDir"
 fi
@@ -35,21 +49,22 @@ do
 done
 classpath=$classpath":"$CLASSPATH
 
-for tc in `ls $AUTTESTCASE`
+
+for tc in `ls $testcaseDir`
 do
     testcase_id=${tc%????}
-    testcase=$AUTTESTCASE"/"$tc
+    testcase=$testcaseDir"/"$tc
     log=$AUTLOG"/"$testcase_id".log"
     sta=$AUTSTA"/"$testcase_id".sta"
     echo "processing "$testcase
     cmd="java -javaagent:./lib/jacocoagent.jar -Dlog4j.configuration=$log4j -cp $classpath edu.umd.cs.guitar.replayer.JFCReplayerMain -c $AUT_MAINCLASS -g $guiFile -e $efgFile -t $testcase -i 500 -d 200 -l $log -gs $sta -cf $configurationFile -ts"
 #    echo $cmd
     eval $cmd
-    mv ./jacoco.exec  $guitarReportJacocoExecDir"/"$tc".jacoco.exec"
+    mv ./jacoco.exec  $reportJacocoExecDir"/"$tc".jacoco.exec"
 done
 
-cp ./conf/jacoco-template.xml $guitarReportDir"/jacoco.xml"
-cd $guitarReportDir
+cp ./conf/jacoco-template.xml $reportDir"/jacoco.xml"
+cd $reportDir
 chmod u+wx "jacoco.xml"
 sed  -i "s/<fileset dir=\"\${result.classes.dir\}\"><\/fileset>/<fileset dir=\"\${result.classes.dir\}\"><include name=\"$AUTJAR\"\/><\/fileset>/g" jacoco.xml
 ant -f "jacoco.xml"
